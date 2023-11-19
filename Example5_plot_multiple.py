f4f6d2eb083ce0344891459c0c56ce18801fc83b
ag00se <johannes.koch@hotmail.com>
2023-11-19 09:38:39 +0100

    * Add 2023 to freedom of press rating

--- Example5_plot_multiple.py
+++ Example5_plot_multiple.py
@@ -11,9 +11,9 @@ class MainWindow(QMainWindow):
         plot.setBackground('w')
 
         # data obtained from: https://rsf.org/en/index
-        year = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
-        freedom_of_press_austria_ranking = [16, 16, 16, 14, 13, 7, 5, 5, 12, 12, 7, 11, 11, 11, 16, 18, 17, 31]
-        freedom_of_press_swiss_ranking = [1, 9, 11, 7, 7, 6, 8, 8, 14, 15, 20, 7, 7, 5, 6, 8, 10, 14]
+        year = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+        freedom_of_press_austria_ranking = [16, 16, 16, 14, 13, 7, 5, 5, 12, 12, 7, 11, 11, 11, 16, 18, 17, 31, 29]
+        freedom_of_press_swiss_ranking = [1, 9, 11, 7, 7, 6, 8, 8, 14, 15, 20, 7, 7, 5, 6, 8, 10, 14, 12]
         
         pen_red = pg.mkPen(color=(255, 0, 0), width=5)              # create a pen to 'draw' the plot (red)
         pen_blue = pg.mkPen(color=(0, 0, 255), width=5)             # create a pen to 'draw' the plot (blue)
